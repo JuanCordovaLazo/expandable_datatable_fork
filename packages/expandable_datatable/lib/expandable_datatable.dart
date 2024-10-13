b8599ebc4c01b3de697f3286572d3e9b6b695fd1
Juan Córdova <juancordovalazo@gmail.com>
2024-10-13 09:41:41 -0500

    Changed exports

--- packages/expandable_datatable/lib/expandable_datatable.dart
+++ packages/expandable_datatable/lib/expandable_datatable.dart
@@ -1,7 +1,13 @@
 library expandable_datatable;
 
+// export 'package:expandable_datatable/expandable_datatable.dart';
+export 'package:expandable_datatable/src/model/expandable_row.dart';
+export 'package:expandable_datatable/src/model/expandable_column.dart';
+export 'package:expandable_datatable/src/utility/expandable_theme.dart';
+export 'package:expandable_datatable/src/utility/expandable_theme_data.dart';
+
 export 'src/expandable_datatable.dart';
-export 'src/model/expandable_row.dart';
-export 'src/model/expandable_column.dart';
-export 'src/utility/expandable_theme.dart';
-export 'src/utility/expandable_theme_data.dart';
+// export 'src/model/expandable_row.dart';
+// export 'src/model/expandable_column.dart';
+// export 'src/utility/expandable_theme.dart';
+// export 'src/utility/expandable_theme_data.dart';
